d6c4697c9d73922fa0f5398e42ee7a79e7422b8f
Debra <debrawangeci@gmail.com>
2025-06-18 10:53:33 +0300

    add new changes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
-const button=document.getElementById('btn');
-button.addEventListener('click',addGUEST);
-
-
 function addGUEST(event){
  event.preventDefault();
 
   
   const guestNameInput=document.getElementById('guest-name');
   const guestlist=document.getElementById('guest-list');
-  const ListNames=document.getElementById('list');
+  const GuestNames=document.getElementById('list');
 
   
   const name=guestNameInput.value;
   if(name===""){
-   ListNames.textContent="please enter guest name!";
+   GuestNamesNames.textContent="please enter guest name!";
    
     return;
 
   }else{
-    ListNames.textContent="";
+    GuestNames.textContent="";
   }
    
   
@@ -30,20 +26,23 @@ function addGUEST(event){
    console.log(name);
 
   
-  const Li=document.createElement('li'); 
-  Li.textContent=name + "";
+  const List=document.createElement('li'); 
+  List.textContent=name + "";
   
   const deleteBtn=document.createElement('button');
   deleteBtn.textContent="Remove";
 
   deleteBtn.addEventListener('click',()=>{
-  guestlist.removeChild(Li); 
+  guestlist.removeChild(List); 
   })
   
-  Li.appendChild(deleteBtn);
-  guestlist.appendChild(Li);
+  List.appendChild(deleteBtn);
+  guestlist.appendChild(List);
   guestNameInput.value="";
 
-  
  
   }
+const button=document.getElementById('btn');
+button.addEventListener('click',addGUEST); 
+
+
